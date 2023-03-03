a2e3266e5af10cee1d7e1a7573d98d50666cb79f
PAOK-2001 <2001.paok@gmail.com>
2023-03-02 23:35:02 -0600

    Integrated accelerometer

--- SensorNetwork/SensorNetwork.ino
+++ SensorNetwork/SensorNetwork.ino
@@ -7,9 +7,14 @@
  */
 
 #include <DS1307Pico.h>
-#define IR 5
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+#define IR 15
 #define FOTO_RESISTOR A0
 
+Adafruit_ADXL345_Unified accelerometer = Adafruit_ADXL345_Unified(6,0,7,1,1234); // Initialize SPI connection with ADXL345, assigning ID 1234
 DS1307Pico rtc; //Creates clock object, reading I2C address from I2C0
 uint32_t lastTime; 
 
@@ -29,8 +34,20 @@ int read_from_photores(){
   
 }
 
+/*
+ * Read data from accelerometer 
+ */
+void read_accelerometer(){
+  sensors_event_t event; 
+  accelerometer.getEvent(&event);
+  // Return data from the 3 axis
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+}
+
 int is_presence(){
-  return digitalRead(IR);
+  return !digitalRead(IR);
 }
 
 void setup() {
@@ -38,6 +55,12 @@ void setup() {
   pinMode(IR, INPUT);
   pinMode(FOTO_RESISTOR, INPUT);
   rtc.begin();
+  if(!accelerometer.begin()){
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+  // Chose sensor lib
+  accelerometer.setRange(ADXL345_RANGE_16_G);
   // Optional setup to set an initial time
   //rtc.DSadjust(22, 40, 0, 2023, 3, 2); // 00:15:21 16 Mar 2022
  
@@ -47,7 +70,9 @@ void loop() {
   if(millis() - lastTime >= 1000) {
     rtc.DSread(); 
     read_time();
+    read_accelerometer();
     Serial.println("Ṕhotoresistor: " + String(read_from_photores()));
+    Serial.println("IR: " + String(is_presence()));
     lastTime = millis();
    }
 
